af689db23c2d18e22cdb9c12f6f5f727bc820084
creusator <elias.morin@hotmail.com>
2024-12-18 10:25:09 +0100

    Added classes Solid() & Liquid()

--- Scripts/Game.py
+++ Scripts/Game.py
@@ -1,5 +1,11 @@
-class blocs :
+class solide :
     def __init__(self):
         self.durete : 0
-        self.is_flamable = False
+        self.is_flammable = False
+        self.sprite = None
+
+class liquide : 
+    def __init__(self):
+        self.damage : 0
+        self.viscosity : 0
         self.sprite = None
