44150c07b82be7a525b1c4bd51fac35fa04ea851
creusator <elias.morin@hotmail.com>
2024-12-20 13:36:14 +0100

    Creation du script bloc pour une meilleure lisibilité.

---
+++ Scripts/Blocs.py
@@ -0,0 +1,49 @@
+import pygame
+
+class Bloc() :
+    def __init__(self, chemin_sprite):
+        self.sprite = self.charger_sprite(chemin_sprite)
+        self.id = None
+
+    def charger_sprite(self, chemin_sprite):
+        """Renvoi un sprite utilisable redimensionné en 64x64"""
+        return pygame.transform.scale(pygame.image.load(chemin_sprite), (64, 64))
+
+    def placer(self):
+        """Place le bloc SUR la GRILLE au coordonées du curseur. """
+
+    def detruire(self):
+        """Notes pour plus tard :
+            Si le bloc est solide --> mettre dans l'inventaire / détruire
+            Si le bloc est liquide et que le joueur à un seau --> mettre dans le seau"""
+
+class Solide(Bloc) :
+    def __init__(self, id, chemin_sprite):
+        super().__init__(chemin_sprite)
+        self.durete : 0
+        self.is_flammable = False
+        self.has_collision = True
+
+class Liquide(Bloc) : 
+    def __init__(self, sprite):
+        super().__init__()
+        self.degats : 0
+        self.viscosite : 0
+
+def stone_block():
+    stone_block = Solide(1, 'Asset/image/Blocs/bloc_stone.png')
+    stone_block.durete = 10
+    stone_block.id = 1
+    return stone_block
+
+def grass_block():
+    grass_block = Solide(1, 'Asset/image/Blocs/bloc_herbe.png')
+    grass_block.durete = 5
+    grass_block.id = 2
+    return grass_block
+
+def dirt_block():
+    dirt_block = Solide(1, 'Asset/image/Blocs/bloc_terre.png')
+    dirt_block.durete = 5
+    dirt_block.id = 3
+    return dirt_block
--- Scripts/Game.py
+++ Scripts/Game.py
@@ -1,35 +1,5 @@
 import pygame
-
-class Bloc() :
-    def __init__(self, chemin_sprite):
-        self.sprite = self.charger_sprite(chemin_sprite)
-        self.id = None
-
-    def charger_sprite(self, chemin_sprite):
-        """Renvoi un sprite utilisable redimensionné en 64x64"""
-        return pygame.transform.scale(pygame.image.load(chemin_sprite), (64, 64))
-
-    def placer(self):
-        """Place le bloc SUR la GRILLE au coordonées du curseur. """
-
-    def detruire(self):
-        """Notes pour plus tard :
-            Si le bloc est solide --> mettre dans l'inventaire / détruire
-            Si le bloc est liquide et que le joueur à un seau --> mettre dans le seau"""
-    
-class Solide(Bloc) :
-    def __init__(self, id, chemin_sprite):
-        super().__init__(chemin_sprite)
-        self.durete : 0
-        self.is_flammable = False
-
-
-class Liquide(Bloc) : 
-    def __init__(self, sprite):
-        super().__init__()
-        self.degats : 0
-        self.viscosite : 0
-
+from Blocs import *
 class Personnage():
     def __init__(self):
         self.coordx = 0
@@ -37,18 +7,6 @@ class Personnage():
         self.vie = 200
         self.armure = 0
         self.vitesse = 10
-
-def stone_block():
-    stone_block = Solide(1, 'Asset/image/Blocs/bloc_stone.png')
-    return stone_block
-
-def grass_block():
-    stone_block = Solide(1, 'Asset/image/Blocs/bloc_herbe.png')
-    return stone_block
-
-def dirt_block():
-    stone_block = Solide(1, 'Asset/image/Blocs/bloc_terre.png')
-    return stone_block
 
 SCREEN_WIDTH = 512
 SCREEN_HEIGHT = 256
@@ -58,8 +16,6 @@ screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 clock = pygame.time.Clock()
 
 running = True
-cobble = Solide(1, 'Asset/image/Blocs/bloc_stone.png')
-
 
 while running:
     for event in pygame.event.get():
