cce71f2262ecda17f01c9c751d8105e545f03553
creusator <elias.morin@hotmail.com>
2024-12-20 14:25:35 +0100

    Map test (pas fini) [code moche] {désolé}

--- Scripts/Game.py
+++ Scripts/Game.py
@@ -8,8 +8,8 @@ class Personnage():
         self.armure = 0
         self.vitesse = 10
 
-SCREEN_WIDTH = 512
-SCREEN_HEIGHT = 256
+SCREEN_WIDTH = 1024
+SCREEN_HEIGHT = 512
 
 pygame.init()
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -22,12 +22,49 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    screen.blit(stone_block().sprite, (0, 0))
-    screen.blit(grass_block().sprite, (64, 0))
-    screen.blit(dirt_block().sprite, (64, 64))
+    screen.blit(stone_block().sprite, (0, 448))
+    screen.blit(stone_block().sprite, (0, 384))
+    screen.blit(grass_block().sprite, (0, 256))
+    screen.blit(dirt_block().sprite, (0, 320))
+    screen.blit(stone_block().sprite, (64, 448))
+    screen.blit(stone_block().sprite, (64, 384))
+    screen.blit(grass_block().sprite, (64, 256))
+    screen.blit(dirt_block().sprite, (64, 320))
+    screen.blit(stone_block().sprite, (128, 448))
+    screen.blit(stone_block().sprite, (128, 384))
+    screen.blit(grass_block().sprite, (128, 256))
+    screen.blit(dirt_block().sprite, (128, 320))
+    screen.blit(stone_block().sprite, (192, 448))
+    screen.blit(stone_block().sprite, (192, 384))
+    screen.blit(grass_block().sprite, (192, 256))
+    screen.blit(dirt_block().sprite, (192, 320))
+    screen.blit(stone_block().sprite, (256, 448))
+    screen.blit(stone_block().sprite, (256, 384))
+    screen.blit(grass_block().sprite, (256, 256))
+    screen.blit(dirt_block().sprite, (256, 320))
+    screen.blit(stone_block().sprite, (320, 448))
+    screen.blit(stone_block().sprite, (320, 384))
+    screen.blit(grass_block().sprite, (320, 256))
+    screen.blit(dirt_block().sprite, (320, 320))
+    screen.blit(stone_block().sprite, (384, 448))
+    screen.blit(stone_block().sprite, (384, 384))
+    screen.blit(grass_block().sprite, (384, 256))
+    screen.blit(dirt_block().sprite, (384, 320))
+    screen.blit(stone_block().sprite, (448, 448))
+    screen.blit(stone_block().sprite, (448, 384))
+    screen.blit(grass_block().sprite, (448, 256))
+    screen.blit(dirt_block().sprite, (448, 320))
+    screen.blit(stone_block().sprite, (512, 448))
+    screen.blit(stone_block().sprite, (512, 384))
+    screen.blit(grass_block().sprite, (512, 256))
+    screen.blit(dirt_block().sprite, (512, 320))
+    screen.blit(stone_block().sprite, (576, 448))
+    screen.blit(stone_block().sprite, (576, 384))
+    screen.blit(grass_block().sprite, (576, 256))
+    screen.blit(dirt_block().sprite, (576, 320))
 
     pygame.display.flip()
     clock.tick(4)
-    screen.fill("White")
+    screen.fill((135,206,235))
 
 pygame.quit()
