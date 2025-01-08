84e8372bb785130975e049a69d9a9eae644d994e
creusator <elias.morin@hotmail.com>
2025-01-08 11:29:07 +0100

    Ajout de l'orientation du joueur en fonction de sa direction

--- Scripts/Personnage.py
+++ Scripts/Personnage.py
@@ -2,7 +2,7 @@ import pygame
 
 class Personnage():
     def __init__(self):
-        self.sprite = self.charger_sprite("Asset/image/personnage/skin de base.png")
+        self.sprite = self.charger_sprite("Asset/image/personnage/skin de base gauche.png")
         self.coordx = 50
         self.coordy = 50
         self.vie = 200
@@ -10,16 +10,18 @@ class Personnage():
         self.vitesse = 256
     
     def charger_sprite(self, chemin_sprite):
-        """Renvoi un sprite utilisable redimensionné en 64x64"""
+        """Renvoi un sprite utilisable redimensionné en 64x128"""
         return pygame.transform.scale(pygame.image.load(chemin_sprite), (64, 128))
 
     def move(self, arg, delta):
         '''Permet d'executer les instructions nécéssaires au déplacement du personnage'''
         if arg == "right":
             self.coordx += self.vitesse * delta
+            self.sprite = self.charger_sprite("Asset/image/personnage/skin de base droite.png")
 
         if arg == "left":
             self.coordx -= self.vitesse * delta
+            self.sprite = self.charger_sprite("Asset/image/personnage/skin de base gauche.png")
         
         if arg == "jump":
             self.coordy -= 20
@@ -27,7 +29,7 @@ class Personnage():
 
     def gravité(self):
         '''Applique la gravité au personnage en fonction des liquides'''
-        self.coordy += 5
+        self.coordy += 0
 
     def afficher(self, screen):
         '''Permet d'afficher le personnage sur l'écran'''
