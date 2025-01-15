d488a2ec5fe6baec5c5d11e7b0cf6b03a246e4e5
creusator <elias.morin@hotmail.com>
2025-01-15 10:33:05 +0100

    Fix collisions

--- Scripts/Personnage.py
+++ Scripts/Personnage.py
@@ -17,43 +17,47 @@ class Personnage():
     def move(self, direction, grille, delta):
         '''Permet d'exécuter les instructions nécessaires au déplacements du personnage'''
         if direction == "right":
-            bloc_grille_bas = grille.get_bloc(grille.get_coord_grille((self.coordx + 32, self.coordy - 16)))
-            bloc_grille_milieu = grille.get_bloc(grille.get_coord_grille((self.coordx + 32, self.coordy - 64)))
-            bloc_grille_haut = grille.get_bloc(grille.get_coord_grille((self.coordx +   32, self.coordy - 128)))
+            bloc_grille_bas = grille.get_bloc(grille.get_coord_grille((self.coordx + 24, self.coordy - 16)))
+            bloc_grille_milieu = grille.get_bloc(grille.get_coord_grille((self.coordx + 24, self.coordy - 64)))
+            bloc_grille_haut = grille.get_bloc(grille.get_coord_grille((self.coordx +   24, self.coordy - 128)))
             if bloc_grille_bas == 0 and bloc_grille_milieu == 0 and bloc_grille_haut == 0 :
                 self.coordx += self.vitesse * delta
                 self.sprite = self.charger_sprite("Asset/image/personnage/skin de base droite.png")
 
         if direction == "left":
-            bloc_grille_bas = grille.get_bloc(grille.get_coord_grille((self.coordx - 32, self.coordy - 16)))
-            bloc_grille_milieu = grille.get_bloc(grille.get_coord_grille((self.coordx - 32, self.coordy - 64)))
-            bloc_grille_haut = grille.get_bloc(grille.get_coord_grille((self.coordx - 32, self.coordy - 128)))
+            bloc_grille_bas = grille.get_bloc(grille.get_coord_grille((self.coordx - 24, self.coordy - 16)))
+            bloc_grille_milieu = grille.get_bloc(grille.get_coord_grille((self.coordx - 24, self.coordy - 64)))
+            bloc_grille_haut = grille.get_bloc(grille.get_coord_grille((self.coordx - 24, self.coordy - 128)))
             if bloc_grille_bas == 0 and bloc_grille_milieu == 0 and bloc_grille_haut == 0 :
                 self.coordx -= self.vitesse * delta
                 self.sprite = self.charger_sprite("Asset/image/personnage/skin de base gauche.png")
 
     def jump(self, grille):
-        bloc_grille_pied = grille.get_bloc(grille.get_coord_grille((self.coordx, self.coordy)))
+        bloc_grille_pied_gauche = grille.get_bloc(grille.get_coord_grille((self.coordx - 16, self.coordy)))
+        bloc_grille_pied_droit = grille.get_bloc(grille.get_coord_grille((self.coordx + 16, self.coordy)))
         bloc_grille_tete = grille.get_bloc(grille.get_coord_grille((self.coordx, self.coordy - 140)))
-        if bloc_grille_pied != 0 and bloc_grille_tete == 0:
-            self.coordy -= 128
+        if bloc_grille_pied_gauche != 0 or bloc_grille_pied_droit != 0 :
+            if bloc_grille_tete == 0:
+                self.coordy -= 128
 
     def gravité(self, grille):
         '''Applique la gravité au personnage en fonction des bloc en dessous'''
-        bloc_grille_pied = grille.get_bloc(grille.get_coord_grille((self.coordx, self.coordy)))
-        if bloc_grille_pied == 0  :
+        bloc_grille_pied_gauche = grille.get_bloc(grille.get_coord_grille((self.coordx - 16, self.coordy)))
+        bloc_grille_pied_droit = grille.get_bloc(grille.get_coord_grille((self.coordx + 16, self.coordy)))
+        if bloc_grille_pied_gauche == 0 and bloc_grille_pied_droit == 0  :
             self.coordy += 10
 
     def debug(self, screen):
         """Affiche à l'écran des graphisme de debug, visualisation des collisions ect..."""
-        screen.blit(self.pos_indicator, (self.coordx, self.coordy)) #Pieds du joueur
+        screen.blit(self.pos_indicator, (self.coordx - 16, self.coordy)) #Pieds gauche du joueur
+        screen.blit(self.pos_indicator, (self.coordx + 16, self.coordy)) #Pieds droit du joueur
         screen.blit(self.pos_indicator, (self.coordx, self.coordy - 140)) #Tête du joueur
-        screen.blit(self.pos_indicator, (self.coordx - 32, self.coordy - 16)) #Bas gauche
-        screen.blit(self.pos_indicator, (self.coordx + 32, self.coordy - 16)) #Bas droit
-        screen.blit(self.pos_indicator, (self.coordx - 32, self.coordy - 64)) #Milieu gauche
-        screen.blit(self.pos_indicator, (self.coordx + 32, self.coordy - 64)) #Milieu droit
-        screen.blit(self.pos_indicator, (self.coordx - 32, self.coordy - 128)) #Haut gauche
-        screen.blit(self.pos_indicator, (self.coordx + 32, self.coordy - 128)) #Haut droit
+        screen.blit(self.pos_indicator, (self.coordx - 24, self.coordy - 16)) #Bas gauche
+        screen.blit(self.pos_indicator, (self.coordx + 24, self.coordy - 16)) #Bas droit
+        screen.blit(self.pos_indicator, (self.coordx - 24, self.coordy - 64)) #Milieu gauche
+        screen.blit(self.pos_indicator, (self.coordx + 24, self.coordy - 64)) #Milieu droit
+        screen.blit(self.pos_indicator, (self.coordx - 24, self.coordy - 128)) #Haut gauche
+        screen.blit(self.pos_indicator, (self.coordx + 24, self.coordy - 128)) #Haut droit
 
     def afficher(self, screen):
         '''Permet d'afficher le personnage sur l'écran'''
